8ebec5a55678fd9d004116bb017d30cc08097b46
qwametekpo <mtekpo@yahoo.com>
2023-08-04 08:35:51 -0700

    argc, argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,9 +11,12 @@ int check_num(char *str)
 {
 	/*Declaring of variables*/
 	unsigned int count;
+
 	count = 0;
+
 	while (count < strlen(str)) /*count string*/
 	{
+
 		if (!isdigit(str[count])) /*check if str there are digit*/
 		{
 			return (0);
